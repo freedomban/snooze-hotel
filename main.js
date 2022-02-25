cd6008d9e2aeff726fed387d595a0e99a3720757
freedomban <xiangenius@gmail.com>
2022-02-25 10:46:41 +0800

    add mob type and hamburger

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ $(() => {
     $('.thumbList .on img').on('click', function () {
       $('.sldimg > img').attr('src', $(this).attr('src'))
       console.log($(this).attr('src'));
-  })
-})
+  });
 
+  // 小漢堡點擊
+  $("button.hamburger").on("click", function(){
+    $(this).toggleClass("is-active");
+  });
 
+  $("button.hamburger").on("click", function(){
+    $(".offcanvas").toggle();
+  });
+
+});
