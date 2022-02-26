62eb0453084bd800da439cbc4d7227399d59f8b9
freedomban <xiangenius@gmail.com>
2022-02-27 03:00:59 +0800

    add RWD , Classification of css, add navbar , common css , revise main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(() => {
-    $('.thumbList .on img').on('click', function () {
+    $('.thumbList .sldimg-block img').on('click', function () {
       $('.sldimg > img').attr('src', $(this).attr('src'))
       console.log($(this).attr('src'));
   });
@@ -7,10 +7,23 @@ $(() => {
   // 小漢堡點擊
   $("button.hamburger").on("click", function(){
     $(this).toggleClass("is-active");
+    $(".offcanvas").toggleClass("on");
+  });
+  //回到最上方
+  $("#goBackBtn").click(function () {
+    // console.log("滑到最上方");
+    const duration = 600;
+    $("html, body").stop().animate({
+        scrollTop: 0
+    }, duration);
+  });
+  // roomintro drop down
+  $(".roommain").on("click", function(){
+    // console.log("aaaaaa");
+    $(".roomdown-menu").toggle();
   });
 
-  $("button.hamburger").on("click", function(){
-    $(".offcanvas").toggle();
-  });
+
+
 
 });
