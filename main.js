f051cf3d40d2fb10b77c1a4fe3ad01e6d9b11137
freedomban <xiangenius@gmail.com>
2022-03-01 09:46:53 +0800

    add tabami ad

--- main.js
+++ main.js
@@ -32,4 +32,19 @@ $(() => {
   });
 
 
+  // 綁定 .ad-toggle-btn 的點擊事件
+$(".ad-toggle-btn").click(function () {
+  // console.log("[.ad-toggle-btn被點擊了]");
+  // 讓.side-box被切換class="active"
+  $(".side-box").toggleClass("active");
+});
+
+// 綁定 #removeSideBoxBtn 的點擊事件
+$("#removeSideBoxBtn").click(function () {
+  console.log("[#removeSideBoxBtn被點擊了]")
+  // $(".side-box").removeClass("active");
+  $(".side-box").fadeOut(500);
+});
+
+
 });
